9adb7fda8809512d0d668d58c6465e0109dcb870
Patrick Curl <pixelgremlins@gmail.com>
2013-09-05 17:22:22 -0400

    test

--- app/views/emails/auth/welcome.blade.php
+++ app/views/emails/auth/welcome.blade.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
   </head>
   <body>
-    <h2>Welcome to TopBookPrices.com</h2>
+    <h2>Welcome to RecycleABook.com</h2>
 
     <p><b>Account:</b> {{{ $email }}}</p>
-    <p>Your account has automagically been activated.</p>
+    <p>Your account has been activated and you're ready to make cold hard cash selling all your old textbooks.</p>
     <p>To login visit:  <br /> {{  URL::to('users/login') }}</p>
-    <p>Be sure to bookmark us and comeback each semester.</p> 
+    <p>Be sure to bookmark us and comeback each semester.</p>
     <p>Our customers save thousands on textbooks.</p>
     <p>Thank you, <br />
-      ~The Admin Team</p>
+      ~The RecycleABook Team</p>
   </body>
 </html>
