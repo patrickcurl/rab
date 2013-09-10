5874f635404a2b4a8245d56226ae53976b40c46c
Patrick Curl <pixelgremlins@gmail.com>
2013-09-10 17:01:33 -0400

    test

--- app/config/packages/juy/profiler/config.php
+++ app/config/packages/juy/profiler/config.php
@@ -3,7 +3,7 @@
 return array(
 
 	// Set to TRUE to enable profiling, FALSE to disable. NULL to listen to the app.debug value (default)
-	'profiler' => NULL,
+	'profiler' => FALSE,
     // Profiler can hide certain footer elements and be annoying. This makes it minimized by default. Set true to enable.
     'minimized' => true
 
