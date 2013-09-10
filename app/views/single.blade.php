e6c14f7b6f4cd1e38dd134bb73bda654b9fccfab
Patrick Curl <pixelgremlins@gmail.com>
2013-09-10 16:42:35 -0400

    test

--- app/views/single.blade.php
+++ app/views/single.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 
 
-<div class="col-md-12" style="margin-top:100px;">
+<div class="col-xs-12" style="margin-top:100px;">
             {{ Form::open(array('action' => 'BookController@postSearchSingle', 'id' => 'price-books-form', 'class' => 'form-inline')) }}
             <div class="container clearfix" style="margin-top: 20px; margin-bottom: 30px;">
-                <div class='col-md-12'>
+                <div class='col-xs-12'>
                     <input name="isbns" type="tel" id="isbns" class="form-control input-lg" placeholder="Enter ISBNs separate by commas no spaces." />
                 </div>
                 <div class='col-xs-12' style="margin-top:2px;">
