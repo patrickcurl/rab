3988991f6a21d46ca798114a6ea504034dc0937e
Patrick Curl <pixelgremlins@gmail.com>
2013-09-10 16:59:31 -0400

    test

--- app/views/single.blade.php
+++ app/views/single.blade.php
@@ -3,16 +3,15 @@
 @section('content')
 
 
-<div class="col-xs-12" style="padding-top:50px;margin-top:100px;">
+<div class="container-fluid" style="padding-top:50px;margin-top:100px;">
             {{ Form::open(array('action' => 'BookController@postSearchSingle', 'id' => 'price-books-form', 'class' => 'form-inline')) }}
-            <div class="container clearfix" >
+
                 <div class='col-xs-12'>
-                    <input name="isbns" type="tel" id="isbns" class="form-control input-lg" placeholder="Enter ISBNs separate by commas no spaces." />
+                    <input name="isbns" type="tel" id="isbns" class="form-control input-lg" placeholder="Enter ISBNs separate by commas no spaces."  id="single-input"/>
                 </div>
-                <div class='col-xs-12' style="margin-top:2px;">
+                <div class='col-xs-12' style="margin-top:5px;">
                     <button type="submit" class="btn btn-danger btn-lg col-xs-12">SELL YOUR BOOKS »</button>
                     </div>
-            </div>
 
 
             {{ Form::close() }}
