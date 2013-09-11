01da72dcc7b11998f245511d0fa9a10ec869c982
Patrick Curl <pixelgremlins@gmail.com>
2013-09-11 12:30:12 -0400

    test

--- app/views/books/search_single.blade.php
+++ app/views/books/search_single.blade.php
@@ -10,21 +10,22 @@
 			    	?>
 			    	<h3 class="alert-warning">{{ substr($book->title, 0, 110) }}...</h3>
 			<div class="col-xs-12">
-			<div class="container">
+			<div class="container-fluid">
 
 					<img src="<?php if ($book->image_url) {echo $book->image_url; } else {echo URL::asset('img/no_image.png'); } ?>" width="200" height="300" class="img-thumbnail img-responsive">
 			</div>
 			</div>
 			<div class="col-xs-12 well" style="margin-top:10px;">
 					<dl class="dl-horizontal" style="display: inline-block !important; vertical-align: middle !important;">
-						<dt class="" style="color:rgb(201, 90, 90);font-size:17px;">Buyback Price:</dt><dd style="color:#317EAC;font-size:17px;font-weight:bold;"><strong>{{ $price }}</strong></dd>
-			            <dt class="hidden-xs">Author:</dt><dd>{{  $book->author }}</dd>
-			            <dt class="hidden-xs">Publisher:</dt><dd>{{  $book->publisher }}</dd>
-			            <dt class="hidden-xs">Edition:</dt><dd class="hidden-xs">{{  $book->edition }}</dd>
-			            <dt class="hidden-xs">Weight:</dt><dd>{{ number_format($book->weight, 2) }} lbs</dd>
-			            <dt class="hidden-xs">ISBN10:</dt><dd>{{ $book->isbn10 }} </dd>
-			            <dt class="hidden-xs">ISBN13:</dt><dd>{{ $book->isbn13 }}</dd>
-			            <dt class="hidden-xs">More info:</dt><dd class="hidden-xs"><a href="{{ $book->amazon_url }}" target="_blank">View Book Details on Amazon</a></dd>
+						<dt class="col-xs-4" style="color:rgb(201, 90, 90);font-size:16px;">Buyback Price:</dt>
+						<dd class="col-xs-5" style="color:#317EAC;font-size:16px;font-weight:bold;margin-left:20px;">{{ $price }}</dd>
+			            <dt class="col-xs-4">Author:</dt><dd class="col-xs-6" style="margin-left:20px;">{{  $book->author }}</dd>
+			            <dt class="col-xs-4">Publisher:</dt><dd class="col-xs-6" style="margin-left:20px;">{{  $book->publisher }}</dd>
+			            <dt class="col-xs-4">Edition:</dt><dd class="col-xs-6" style="margin-left:20px;">{{  $book->edition }}</dd>
+			            <dt class="col-xs-4">Weight:</dt><dd class="col-xs-6" style="margin-left:20px;">{{ number_format($book->weight, 2) }} lbs</dd>
+			            <dt class="col-xs-4">ISBN10:</dt><dd class="col-xs-6" style="margin-left:20px;">{{ $book->isbn10 }} </dd>
+			            <dt class="col-xs-4">ISBN13:</dt><dd class="col-xs-6" style="margin-left:20px;">{{ $book->isbn13 }}</dd>
+			            <dt class="col-xs-4">More info:</dt><dd class="col-xs-6" style="margin-left:20px;"><a href="{{ $book->amazon_url }}" target="_blank">View Book Details on Amazon</a></dd>
 			        </dl>
 </div>
 
