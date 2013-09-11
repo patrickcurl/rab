91c85ae487f777e01bba71a718ae76dbf6bf3853
Patrick Curl <pixelgremlins@gmail.com>
2013-09-11 16:39:43 -0400

    test

--- app/controllers/BookController.php
+++ app/controllers/BookController.php
@@ -63,9 +63,44 @@ class BookController extends BaseController {
 
             $price = number_format(($tempbook->Price * 1.7), 2);
 
-            $string ="<books><details><title>{$book->title}</title><author>{$book->author}</author><image>{$book->image_url}</image><isbn>{$book->isbn13}</isbn><buyback>{$price}</buyback></details></books>";
-            $xml = new SimpleXMLElement($string);
-            return $xml->asXML();
+            // $string ="<books><details><title>{$book->title}</title><author>{$book->author}</author><image>{$book->image_url}</image><isbn>{$book->isbn13}</isbn><buyback>{$price}</buyback></details></books>";
+            // $xml = new SimpleXMLElement($string);
+            //
+            // return echo $xml->asXML();
+
+            $dom = new DOMDocument( "1.0", "ISO-8859-15" );
+            $root = $dom->createElement("books");
+            $dom->appendChild($root);
+            $dom->formatOutput=false;
+
+            $details = $dom->createElement("details");
+            $root->appendChild($details);
+            $title_text = $dom->createTextNode($book->title);
+            $title = $dom->createElement("title");
+            $details->appendChild($title_text);
+            $title->appendChild($title_text);
+
+            $author_text = $dom->createTextNode($book->author);
+            $author = $dom->createElement("author");
+            $details->appendChild($author);
+            $author->appendChild($author_text);
+
+            $image_src = $dom->createTextNode($book->image_url);
+            $image = $dom->createElement("image");
+            $image->appendChild($image_src);
+            $details->appendChild($image);
+
+            $isbn_text = $dom->createTextNode($book->isbn13);
+            $isbn = $dom->createElement("isbn");
+            $details->appendChild($isbn);
+            $isbn->appendChild($isbn_text);
+
+            $buyback_text = $dom->createTextNode($price);
+            $offer = $dom->createElement("buyback");
+            $details->appendChild($offer);
+            $offer->appendChild($buyback_text);
+            $output = $dom->saveXML();
+            return $output;
         }
         else
         {
