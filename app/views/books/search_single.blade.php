0cbf9ef74f05bfc86d237231f4b94f30c4d68ecb
Patrick Curl <pixelgremlins@gmail.com>
2013-09-18 15:23:06 -0400

    test

--- app/views/books/search_single.blade.php
+++ app/views/books/search_single.blade.php
@@ -7,7 +7,7 @@ class="container-fluid page-content"
 
     {{ Form::open(array('action' => 'BookController@postSearchSingle', 'id' => 'price-books-form', 'class' => 'form-inline')) }}
       <div class='col-xs-12' style="margin-top:10px;">
-		    <input name="isbns" type="tel" id="isbns" class="form-control input-lg" placeholder="Enter ISBNs separate by commas no spaces."  id="single-input"/>
+		    <input name="isbns" type="number" id="isbns" class="form-control input-lg" placeholder="Enter ISBNs separate by commas no spaces."  id="single-input"/>
       </div>
       <div class='col-xs-12' style="margin-top:5px;">
           <button type="submit" class="btn btn-cart btn-lg col-xs-12">Check Prices »</button>
