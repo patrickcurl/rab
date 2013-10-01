1a9459e8737425544c9a4148bc01cff19735fd0f
Patrick Curl <pixelgremlins@gmail.com>
2013-10-01 15:27:03 -0400

    test

--- app/views/errors/404.blade.php
+++ app/views/errors/404.blade.php
@@ -1,1 +1,12 @@
-test
+@extends('layouts.master')
+@section('hero-start')
+Join Our
+@stop
+@section('hero-end')
+Team
+@stop
+
+@section('content')
+
+<h1>Whoops, I seem to be lost...I just cannot find that page.</h1>
+@stop
