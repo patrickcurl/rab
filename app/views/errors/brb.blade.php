8467fada5fc25a7fd1495a8abf9820091329e130
Patrick Curl <pixelgremlins@gmail.com>
2013-10-08 10:29:54 -0400

    test

---
+++ app/views/errors/brb.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.master')
+@section('hero-start')
+Scheduled
+@stop
+
+@section('hero-end')
+Maintenance
+@stop
+
+@section('content')
+
+<div class="row-fluid">
+<div class="span5">
+	<img src="{{URL::asset('images/brb.jpg')}}">
+</div>
+<div class="span5">
+
+	<p style="margin-top:50px;">The minions are busy updating the site, we will be back shortly!</p>
+</div>
+</div>
+@stop
