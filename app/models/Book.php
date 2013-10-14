22ecfa04247d76bd251f39741e2e3a4428ccadfa
Patrick Curl <pixelgremlins@gmail.com>
2013-10-14 15:12:53 -0400

    test

--- app/models/Book.php
+++ app/models/Book.php
@@ -23,6 +23,33 @@ class Book extends Eloquent {
       }
 
     }
+
+    public function getIsbns($isbn){
+      $isbns = array();
+      $iLength = strlen($isbn);
+      if ($iLength == 10 || $iLength == 13){
+        switch($isbn){
+          case ($iLength == 10):
+            $isbns[10] = $isbn;
+            $isbns[13] = isbn10to13($isbn)
+            break;
+          case ($iLength == 13)
+            $isbns[10] = isbn13to10($isbn)
+            $isbns[13] = $isbn
+            break;
+          default:
+           $isbns[10] = null;
+           $isbns[13] = null
+        }
+
+      }
+      if(isset($isbns[10]) || isset($isbns[13])){
+        return $isbns;
+      } else {
+        return null;
+      }
+    }
+
     // public function getSlugAttribute(){
     //   if($this->isbn13 != null){
     //     $isbn = $this->isbn13;
@@ -31,6 +58,8 @@ class Book extends Eloquent {
     //   }
     //   return $isbn . ' ' . $this->title . ' ' . $this->author . ' '. $this->publisher;
     // }
+
+
     public static function getBook($isbn){
       $region = "com";
       $method = 'GET';
@@ -76,8 +105,8 @@ class Book extends Eloquent {
         return null;
       } else {
         $book = array(
-                    'isbn10' => (string) $xml->Items->Item->ItemAttributes->ISBN,
-                    'isbn13' => (string) $xml->Items->Item->ItemAttributes->EAN,
+                   // 'isbn10' => (string) $xml->Items->Item->ItemAttributes->ISBN,
+                   // 'isbn13' => (string) $xml->Items->Item->ItemAttributes->EAN,
                     'title' => (string) $xml->Items->Item->ItemAttributes->Title,
                     'author' => (string) $xml->Items->Item->ItemAttributes->Author,
                     'publisher' => (string) $xml->Items->Item->ItemAttributes->Publisher,
@@ -121,7 +150,8 @@ class Book extends Eloquent {
              if (isset($book_info))
               // check to see if amazon returned book data
               {
-                  $isbns = array('10' => $book_info['isbn10'], '13' => $book_info['isbn13']);
+                  // $isbns = array('10' => $book_info['isbn10'], '13' => $book_info['isbn13']);
+                  $isbns = getIsbns($isbn);
                   $book = self::bookExists($isbns);
 
                   if($book == null){
@@ -227,5 +257,109 @@ class Book extends Eloquent {
     }
 
 
+  }
+          /**
+  * Function accepts either 12 or 13 digit number, and either provides or checks the validity of the 13th checksum digit
+  *    Optionally converts to ISBN 10 as well.
+  */
+  function isbn13checker($input, $convert = FALSE){
+    $output = FALSE;
+    if (strlen($input) < 12){
+      $output = array('error'=>'ISBN too short.');
+    }
+    if (strlen($input) > 13){
+      $output = array('error'=>'ISBN too long.');
+    }
+    if (!$output){
+      $runningTotal = 0;
+      $r = 1;
+      $multiplier = 1;
+      for ($i = 0; $i < 13 ; $i++){
+        $nums[$r] = substr($input, $i, 1);
+        $r++;
+      }
+      $inputChecksum = array_pop($nums);
+      foreach($nums as $key => $value){
+        $runningTotal += $value * $multiplier;
+        $multiplier = $multiplier == 3 ? 1 : 3;
+      }
+      $div = $runningTotal / 10;
+      $remainder = $runningTotal % 10;
+
+      $checksum = $remainder == 0 ? 0 : 10 - substr($div, -1);
+
+      $output = array('checksum'=>$checksum);
+      $output['isbn13'] = substr($input, 0, 12) . $checksum;
+      if ($convert){
+        $output['isbn10'] = isbn13to10($output['isbn13']);
+      }
+      if (is_numeric($inputChecksum) && $inputChecksum != $checksum){
+        $output['error'] = 'Input checksum digit incorrect: ISBN not valid';
+        $output['input_checksum'] = $inputChecksum;
+      }
+    }
+    return $output;
+  }
+
+  /**
+  * Function accepts either 10 or 9 digit number, and either provides or checks the validity of the 10th checksum digit
+  *    Optionally converts to ISBN 13 as well.
+  */
+  function isbn10checker($input, $convert = FALSE){
+    $output = FALSE;
+    if (strlen($input) < 9){
+      $output = array('error'=>'ISBN too short.');
+    }
+    if (strlen($input) > 10){
+      $output = array('error'=>'ISBN too long.');
+    }
+    if (!$output){
+      $runningTotal = 0;
+      $r = 1;
+      $multiplier = 10;
+      for ($i = 0; $i < 10 ; $i++){
+        $nums[$r] = substr($input, $i, 1);
+        $r++;
+      }
+      $inputChecksum = array_pop($nums);
+      foreach($nums as $key => $value){
+        $runningTotal += $value * $multiplier;
+        //echo $value . 'x' . $multiplier . ' + ';
+        $multiplier --;
+        if ($multiplier === 1){
+          break;
+        }
+      }
+      //echo ' = ' . $runningTotal;
+      $remainder = $runningTotal % 11;
+      $checksum = $remainder == 1 ? 'X' : 11 - $remainder;
+      $checksum = $checksum == 11 ? 0 : $checksum;
+      $output = array('checksum'=>$checksum);
+      $output['isbn10'] = substr($input, 0, 9) . $checksum;
+      if ($convert){
+        $output['isbn13'] = isbn10to13($output['isbn10']);
+      }
+      if ((is_numeric($inputChecksum) || $inputChecksum == 'X') && $inputChecksum != $checksum){
+        $output['error'] = 'Input checksum digit incorrect: ISBN not valid';
+        $output['input_checksum'] = $inputChecksum;
+      }
+    }
+    return $output;
+  }
+
+  function isbn10to13($isbn10){
+
+    $isbnStem = strlen($isbn10) == 10 ? substr($isbn10, 0,9) : $isbn10;
+    $isbn13data = isbn13checker('978' . $isbnStem);
+    return $isbn13data['isbn13'];
+
+  }
+
+  function isbn13to10($isbn13){
+
+    $isbnStem = strlen($isbn13) == 13 ? substr($isbn13, 12) : $isbn13;
+    $isbnStem = substr($isbn13, -10);
+    $isbn10data = isbn10checker($isbnStem);
+    return $isbn10data['isbn10'];
   }
 }
--- app/views/test.blade.php
+++ app/views/test.blade.php
@@ -4,8 +4,13 @@
 
 $isbn = "1558102582";
 $isbn2 = "9780131367739";
+
+?>
+ISBN 13: {{ isbn10to13($isbn) }}
+ISBN 10: {{ isbn13to10($isbn2) }}
+<?php
 $book = getBook($isbn);
-var_dump($book);
+// var_dump($book->Items);
  function getBook($isbn){
       $region = "com";
       $method = 'GET';
@@ -44,7 +49,7 @@ var_dump($book);
       $url = 'http://'.$host.$uri.'?'.$canonicalized_query.'&Signature='.$signature;
       $xml = simplexml_load_file($url);
       $xml->registerXpathNamespace("xmlns", "http://webservices.amazon.com/AWSECommerceService/2011-08-01");
-
+      return $xml;
       if(isset($xml->Items->Request->Errors)){
         return null;
       }
@@ -68,6 +73,113 @@ var_dump($book);
       }
 
     }
+
+
+
+        /**
+  * Function accepts either 12 or 13 digit number, and either provides or checks the validity of the 13th checksum digit
+  *    Optionally converts to ISBN 10 as well.
+  */
+  function isbn13checker($input, $convert = FALSE){
+    $output = FALSE;
+    if (strlen($input) < 12){
+      $output = array('error'=>'ISBN too short.');
+    }
+    if (strlen($input) > 13){
+      $output = array('error'=>'ISBN too long.');
+    }
+    if (!$output){
+      $runningTotal = 0;
+      $r = 1;
+      $multiplier = 1;
+      for ($i = 0; $i < 13 ; $i++){
+        $nums[$r] = substr($input, $i, 1);
+        $r++;
+      }
+      $inputChecksum = array_pop($nums);
+      foreach($nums as $key => $value){
+        $runningTotal += $value * $multiplier;
+        $multiplier = $multiplier == 3 ? 1 : 3;
+      }
+      $div = $runningTotal / 10;
+      $remainder = $runningTotal % 10;
+
+      $checksum = $remainder == 0 ? 0 : 10 - substr($div, -1);
+
+      $output = array('checksum'=>$checksum);
+      $output['isbn13'] = substr($input, 0, 12) . $checksum;
+      if ($convert){
+        $output['isbn10'] = isbn13to10($output['isbn13']);
+      }
+      if (is_numeric($inputChecksum) && $inputChecksum != $checksum){
+        $output['error'] = 'Input checksum digit incorrect: ISBN not valid';
+        $output['input_checksum'] = $inputChecksum;
+      }
+    }
+    return $output;
+  }
+
+  /**
+  * Function accepts either 10 or 9 digit number, and either provides or checks the validity of the 10th checksum digit
+  *    Optionally converts to ISBN 13 as well.
+  */
+  function isbn10checker($input, $convert = FALSE){
+    $output = FALSE;
+    if (strlen($input) < 9){
+      $output = array('error'=>'ISBN too short.');
+    }
+    if (strlen($input) > 10){
+      $output = array('error'=>'ISBN too long.');
+    }
+    if (!$output){
+      $runningTotal = 0;
+      $r = 1;
+      $multiplier = 10;
+      for ($i = 0; $i < 10 ; $i++){
+        $nums[$r] = substr($input, $i, 1);
+        $r++;
+      }
+      $inputChecksum = array_pop($nums);
+      foreach($nums as $key => $value){
+        $runningTotal += $value * $multiplier;
+        //echo $value . 'x' . $multiplier . ' + ';
+        $multiplier --;
+        if ($multiplier === 1){
+          break;
+        }
+      }
+      //echo ' = ' . $runningTotal;
+      $remainder = $runningTotal % 11;
+      $checksum = $remainder == 1 ? 'X' : 11 - $remainder;
+      $checksum = $checksum == 11 ? 0 : $checksum;
+      $output = array('checksum'=>$checksum);
+      $output['isbn10'] = substr($input, 0, 9) . $checksum;
+      if ($convert){
+        $output['isbn13'] = isbn10to13($output['isbn10']);
+      }
+      if ((is_numeric($inputChecksum) || $inputChecksum == 'X') && $inputChecksum != $checksum){
+        $output['error'] = 'Input checksum digit incorrect: ISBN not valid';
+        $output['input_checksum'] = $inputChecksum;
+      }
+    }
+    return $output;
+  }
+
+  function isbn10to13($isbn10){
+
+    $isbnStem = strlen($isbn10) == 10 ? substr($isbn10, 0,9) : $isbn10;
+    $isbn13data = isbn13checker('978' . $isbnStem);
+    return $isbn13data['isbn13'];
+
+  }
+
+  function isbn13to10($isbn13){
+
+    $isbnStem = strlen($isbn13) == 13 ? substr($isbn13, 12) : $isbn13;
+    $isbnStem = substr($isbn13, -10);
+    $isbn10data = isbn10checker($isbnStem);
+    return $isbn10data['isbn10'];
+  }
 // $isbns = array('10' => '0440243831', '13' => '9780440243830');
 // foreach($isbns as $iNum)
 //                   {
