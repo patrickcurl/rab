0e276b9cad12240ea710a5ccf782f117cdaf027b
Patrick Curl <pixelgremlins@gmail.com>
2013-10-31 11:38:27 -0400

    test

--- app/routes.php
+++ app/routes.php
@@ -39,105 +39,39 @@ Route::get('/', function()
 });
 Route::get('pdf/{id}', function($id){
     Config::set('laravel-debugbar::enabled', false);
-    if(!Sentry::check()){return View::make('errors.403');} else{
-        $data = array();
         $order =  Order::find($id);
-        $data['order'] = $order;
-        $data['user'] = $order->user;
-        $data['items'] = DB::table('items')->join('books', function($join){$join->on('books.id', '=', 'items.book_id');})->where('order_id','=',$id)->get();
-        $data['orderTotal'] = number_format($order->total_amount, 2);
-        $label = imagecreatefromstring(base64_decode($order->ups_label));
-        $rotated_imaged = imagerotate($label, -90, 0);
-        $label_path = "uploads/label-{$order->id}.png";
-        imagepng($rotated_imaged, public_path($label_path));
-        $data['label'] = $label_path;
+        if(Sentry::check())
+        {
+            $cUser = Sentry::getUser();
+            if($cUser->hasAccess('admin') || $cUser->id == $order->user->id)
+            {
+                $data = array();
 
-
-
-
-
-
-        //return View::make('orders.print', $data);
-        //download('orderNumber'.$id.'.pdf')
-        return PDF::loadView("orders.print", $data)->stream("OrderNumber_{$order->id}.pdf");
-    }
-
-
-
-    $pdf = PDF::loadHTML('<h1>Test</h1>');
-    return $pdf->download('test.pdf');
-    $input = "http://en.wikipedia.org/w/index.php?title=Jakarta&printable=yes";
-    // $output = new TempFile();
-    // $instance = new Converter(new PhantomJS(), $input, $output);
-    // $output->save(public_path('uploads/file.pdf'));
-
-   // return PDF::url($input);
-});
-Route::get('print/{id}', function($id){
-    Config::set('laravel-debugbar::enabled', false);
-    if(!Sentry::check()){return View::make('errors.403');} else{
-        $data = array();
-        $order =  Order::find($id);
-        $data['order'] = $order;
-        $data['user'] = $order->user;
-        $data['items'] = DB::table('items')->join('books', function($join){$join->on('books.id', '=', 'items.book_id');})->where('order_id','=',$id)->get();
-        $data['orderTotal'] = number_format($order->total_amount, 2);
-       // $ups_label = "";
-
-        define('ORDERS_DIR', public_path('uploads/orders')); // I define this in a constants.php file
-
-        if (!is_dir(ORDERS_DIR)){
-            mkdir(ORDERS_DIR, 0755, true);
+                $data['order'] = $order;
+                $data['user'] = $order->user;
+                $data['items'] = DB::table('items')->join('books', function($join){$join->on('books.id', '=', 'items.book_id');})->where('order_id','=',$id)->get();
+                $data['orderTotal'] = number_format($order->total_amount, 2);
+                $label = imagecreatefromstring(base64_decode($order->ups_label));
+                $rotated_imaged = imagerotate($label, -90, 0);
+                $label_path = "uploads/label-{$order->id}.png";
+                imagepng($rotated_imaged, public_path($label_path));
+                        $data['label'] = $label_path;
+                return PDF::loadView("orders.print", $data)->stream("OrderNumber_{$order->id}.pdf");
+            } else
+            {
+                return Redirect::to('users/login')->with('message', 'You are not authorized to view that label.');
+            }
+        } else
+        {
+            return Redirect::to('users/login')->with('message', 'You must be logged in to print labels');
         }
 
-        $pdfPath = ORDERS_DIR.'/'.$order->id.'.pdf';
-/*
-        $html = "<html><head><style>.page { page-break-before:always; }</style></head><body><div class='page-first'><img src='data:image/gif;base64,$order->ups_label' width='651' style='transform: rotate(90deg);-webkit-transform: rotate(90deg); -ms-transform: rotate(90deg);-moz-transform: rotate(90deg);margin-left:551px;margin-top:100px;'/></div>
-
-            <div class='page'><div class=\"row-fluid\">
-<h1>Packing Slip</h1>
-<table>
-  <tr>
-    <td style=\"padding-right:20px;\"><strong>Ship To:</strong></td>
-    <td style=\"width: 400px\">
-                <br />RecycleABook.com
-                <br />Attn: Chris Harbaugh
-                <br />561 Congress Park Dr
-                <br />Dayton, OH 45459</td>
-                </td>
-    <td style=\"padding-right:20px;\"><strong>Ship From:</strong></td>
-    <td>
-               <br /> $user->first_name $user->last_name
-                <br />$user->address
-                <br />$user->city, $user->state   $user->zip </td>
-
-</tr></table>
-
-
-<table class=\"table container col-md-12\">
-                  <tr>
-                    <td>Book</td>
-                    <td>QTY</td>
-                    <td>Price</td>
-                  </tr>
-
-              </table>
-
-</div>
-<div class=\"well offset8 span2\"><strong>Total: </strong>$ <?php // echo $orderTotal; ?></div>
-
-@endif</div></body></html>";
-*/
-       // File::put($pdfPath, PDF::load($html, 'A4', 'portrait')->output());
-        return View::make('orders.print', $data);
-       // return Redirect::to(URL::to($url));
-        // return PDF::load($html, 'A4', 'portrait')->show();
-    }
 
 
 
 
 });
+
 // Route::get('p/join-our-team', function(){
 //     return Redirect::to('p/join_our_team');
 // });
--- test.rb
+++ test.rb
@@ -10,3 +10,32 @@ def fib(n)
 		return num.to_i
 	end
 end
+
+arr=[
+		"Hello,ell",
+		"This is good,is",
+		"CodeEval,C*Eval",
+		"Old,Young",
+	]
+
+def subSearch(str)
+	cArr = str.split(",") # comparison array
+	a = cArr[0];
+	b = cArr[1]
+	b = b.split("")
+	b.each_with_index do |x,i|
+		if x == "*" then
+			b[i] = ".*"
+		end
+	end
+	b = b.join("")
+	if a.scan(/#{b}/).length > 0 then
+		puts true
+	else
+		puts false
+	end
+end
+
+def pokerHands(hands)
+
+end
