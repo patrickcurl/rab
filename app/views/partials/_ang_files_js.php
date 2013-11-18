9d2406114b0696036c2384645532d5b79bb58e1a
Patrick Curl <pixelgremlins@gmail.com>
2013-11-18 16:15:01 -0500

    test

--- app/views/partials/_ang_files_js.php
+++ app/views/partials/_ang_files_js.php
@@ -47,29 +47,5 @@ app.controller('Ctrl', function($scope) {
     name: 'awesome user'
   };
 });
-$(document).ready(function() {
-    //toggle `popup` / `inline` mode
-   // $.fn.editable.defaults.mode = 'inline';
 
-    //make username editable
-    $('#username').editable();
-
-    //make status editable
-    $('#status').editable({
-        type: 'select',
-        title: 'Select status',
-        placement: 'right',
-        value: 2,
-        source: [
-            {value: 1, text: 'status 1'},
-            {value: 2, text: 'status 2'},
-            {value: 3, text: 'status 3'}
-        ]
-        /*
-        //uncomment these lines to send data on server
-        ,pk: 1
-        ,url: '/post'
-        */
-    });
-});
 </script>
