912147d10cb2c98ad7af8fa2a4a84374aaad4ddf
Patrick Curl <pixelgremlins@gmail.com>
2013-12-09 12:15:01 -0500

    test

--- public/test.php
+++ public/test.php
@@ -1,3 +1,8 @@
 <?php
  echo "test1234go2hellokitty";
+var_dump($_POST);
 ?>
+<form method="POST" action="http://textbooks911.com/test.php">
+<input type="text" name="test" />
+<input type="submit">
+</form>
