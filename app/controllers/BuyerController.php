71929220818391fee3a9f5a87e42692d89e64aac
Patrick Curl <pixelgremlins@gmail.com>
2014-02-06 15:02:29 -0500

    test

--- app/controllers/BuyerController.php
+++ app/controllers/BuyerController.php
@@ -1,17 +1,20 @@
 <?php
 
 class BuyerController extends BaseController {
-
+    public function __construct(){
+        $this->beforeFilter("buyer_auth");
+    }
 
 	public function getDocs()
 	{
 		$user = Sentry::getUser();
-		$data['files'] = $user->docs();
+		$data['files'] = $user->docs()->get();
+
         // If this errors - make sure that 'users' => array ( .. 'model' => 'User' ...)
         // is in app/config/packages/cartalyst/sentry/config.php and that User model Extends Sentry.
         // If file doesn't exist run php artisan config:publish Cartalyst/Sentry
-        //return var_dump($data['files']);
-		return View::make('single.docs', $data);
+
+        return View::make('single.docs', $data);
 	}
 
 }
